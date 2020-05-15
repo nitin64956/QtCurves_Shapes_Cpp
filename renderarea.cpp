a53f14a15b6c945ec84d7cf7ee1c29b73973699d
nitin64956 <nitin64956@gmail.com>
2020-05-15 13:06:29 +0530

    Shape creating Logic added

--- renderarea.cpp
+++ renderarea.cpp
@@ -1,6 +1,7 @@
 #include "renderarea.h"
 #include <QPaintEvent>
 #include <QPainter>
+#include <math.h>
 
 RenderArea::RenderArea(QWidget *parent) :
     QWidget(parent),
@@ -8,6 +9,35 @@ RenderArea::RenderArea(QWidget *parent) :
     mShapeColor  (255,255,255),
     mShape(Asteroid)
 {
+    on_shape_changed();
+}
+
+void RenderArea::on_shape_changed () {
+
+    switch (mShape) {
+        case Asteroid:
+            mScale = 40;
+            mIntervalLength = 2*M_PI;
+            mStepcount = 256;
+            break;
+        case Cycloid:
+            break;
+        case HuygensCycloid:
+            break;
+        case HypoCycloid:
+            break;
+        default:
+            break;
+
+    }
+}
+
+QPointF RenderArea::compute_asteroid (float t) {
+    float cos_t = cos (t);
+    float sint_t = sin (t);
+    float x = 2*cos_t*cos_t*cos_t;
+    float y = 2*sint_t*sint_t*sint_t;
+    return QPointF(x,y);
 
 }
 
@@ -26,23 +56,6 @@ void RenderArea::paintEvent(QPaintEvent *event) {
     QPainter painter(this);
     painter.setRenderHint(QPainter::Antialiasing,true);
 
-    switch (mShape) {
-        case Asteroid:
-            mBackgroundColor = Qt::red;
-            break;
-        case Cycloid:
-            mBackgroundColor = Qt::green;
-            break;
-        case HuygensCycloid:
-            mBackgroundColor = Qt::blue;
-            break;
-        case HypoCycloid:
-            mBackgroundColor = Qt::yellow;
-            break;
-        default:
-            break;
-
-    }
 
     painter.setBrush(mBackgroundColor);
     painter.setPen(mShapeColor);
@@ -50,6 +63,17 @@ void RenderArea::paintEvent(QPaintEvent *event) {
 
     //drawing area
     painter.drawRect(this->rect());
-    painter.drawLine(this->rect().topLeft(),this->rect().bottomRight());
 
+    QPoint center = this->rect().center();
+    float step = mIntervalLength/mStepcount;
+    for (float t=0;t<mIntervalLength;t +=step)
+    {
+            QPointF point = compute_asteroid(t);
+            QPoint pixel;
+            pixel.setX(point.x()*mScale + center.x());
+            pixel.setY(point.y()*mScale + center.y());
+
+            painter.drawPoint(pixel);
+
+    }
 }
--- renderarea.h
+++ renderarea.h
@@ -18,7 +18,7 @@ public:
     }
     QColor backgroundColor () const {return mBackgroundColor;}
 
-    void setShape (ShapeType shape) {mShape = shape;}
+    void setShape (ShapeType shape) {mShape = shape; on_shape_changed();}
     ShapeType shape () const {return mShape;}
 
 protected:
@@ -27,9 +27,17 @@ protected:
 signals:
 
 private:
+    void on_shape_changed();
+    QPointF compute_asteroid(float t);
+
+private:
     QColor mBackgroundColor;
     QColor mShapeColor;
     ShapeType mShape;
+    float mIntervalLength;
+    float mScale;
+    int mStepcount;
+
 
 };
 
